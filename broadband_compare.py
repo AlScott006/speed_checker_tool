4dcc4d864f6c02b9c6c905a4e488c1a1abfc50b3
alex.slingsby <alexslingsby@googlemail.com>
2019-07-06 20:24:47 +0100

    :construction: Added exception if failed to find value in database - Added / fixed report generation function such that the user can specify a large or small report.

--- broadband_compare.py
+++ broadband_compare.py
@@ -14,6 +14,7 @@ ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 """Dataclass for typehints such that it's obvious what's coming in from the CLI arguments"""
 @dataclass
 class Inputdata:
+    report: str = ""
     location: str = ""
     comparison_location: str = ""
     max_dl_speed: int = 0
@@ -29,8 +30,8 @@ translations = {"location": "laua_name",
                 "max_ul_speed": "Maximum upload speed (Mbit/s)",
                 "min_ul_speed": "Minimum upload speed (Mbit/s)",
                 "avg_dl_speed": "Average download speed (Mbit/s)",
+                "avg_ul_speed": "Average upload speed (Mbit/s)",
                 }
-
 
 class Database:
     def __init__(self, dataset: str):
@@ -60,7 +61,12 @@ class Database:
                 t = (where,)
                 sql = f"SELECT * FROM dataset WHERE \"{translations.get(name)}\" = ? COLLATE NOCASE;"
                 c.execute(sql, t)
-                return c.fetchall()[0]
+                result = c.fetchall()
+                try:
+                    return result[0]
+                except IndexError:
+                    raise Exception(f"{where} not found in provided CSV")
+
             else:
                 sql = f"SELECT \"{translations.get(name)}\" FROM dataset"
                 c.execute(sql)
@@ -89,41 +95,41 @@ def main(user_data):
 
     location = database.get_data(name="location", where=user_data.location)
 
-    dl_speed_all = database.get_data(name="avg_dl_speed")
-    avg = calc_avg(dl_speed_all)
-
-
-    if(user_data.max_dl_speed):
+    print(f"Your location: '{user_data.location.capitalize()}': download speed {user_data.max_dl_speed}Mb/s\n")
+    if(user_data.max_dl_speed or user_data.report == "full"):
+        dl_speed_all = database.get_data(name="avg_dl_speed")
         print(
-            f"Your location: {user_data.location}': download speed {user_data.max_dl_speed}Mb/s\n"
-            f"Average download speed of country: {int(avg)}Mb/s.\n"
-            f"Average download speed of {user_data.location}: {get_value(location, 'avg_dl_speed')}Mb/s\n")
+            f"Average download speed of country: {int(calc_avg(dl_speed_all))}Mb/s.\n"
+            f"Average download speed of {user_data.location.capitalize()}: {get_value(location, 'avg_dl_speed')}Mb/s\n")
 
-    if(user_data.max_ul_speed):
+    if(user_data.max_ul_speed or user_data.report == "full"):
+        ul_speed_all = database.get_data(name="avg_ul_speed")
         print(
-            f"Some information about upload speed\n"
+            f"Average upload speed of country: {int(calc_avg(ul_speed_all))}Mb/s.\n"
+            f"Average upload speed of {user_data.location.capitalize()}: {get_value(location, 'avg_ul_speed')}Mb/s\n"
         )
 
     if(user_data.comparison_location):
         compare_location = database.get_data(name="location", where=user_data.comparison_location)
         print(
-            f"Your location: {user_data.location}': download speed {user_data.max_dl_speed}Mb/s\n"
             f"Average download speed of {user_data.comparison_location}: {get_value(compare_location,'avg_dl_speed')}Mb/s")
 
 
 def parse_args():
     import argparse
     parser = argparse.ArgumentParser(description="Internet speed and bandwidth comparison tool")
+    parser.add_argument("-f", dest="csv", help="location of the CSV to load", required=True, type=str)
     parser.add_argument("-l", "--location", dest="location", help="The location to perform internet "
-                                                                  "analysis. E.g. Leicester", type=str)
+                                                                  "analysis. E.g. Leicester", type=str, required=True)
     parser.add_argument("-d", "--max_download_speed", dest="max_dl_speed", help="Maximum download speed Mb/s "
-                                                                                "this connection e.g 50")
-    parser.add_argument("--min_download_speed", dest="min_dl_speed", help="Minumum speed in Mb/s.")
-    parser.add_argument("-u", "--max_upload_speed", dest="max_ul_speed", help="Maximum upload speed in Mb/s")
-    parser.add_argument("--min_upload_speed", dest="max_ul_speed", help="Maximum upload speed in Mb/s")
+                        "this connection e.g 50", type=int)
+    parser.add_argument("--min_download_speed", dest="min_dl_speed", help="Minumum speed in Mb/s.", type=int)
+    parser.add_argument("-u", "--max_upload_speed", dest="max_ul_speed", help="Maximum upload speed in Mb/s", type=int)
+    parser.add_argument("--min_upload_speed", dest="max_ul_speed", help="Maximum upload speed in Mb/s", type=int)
 
-    parser.add_argument("--compare", dest="comparison_location", type=str, help="If supplied will compare your location "
-                                                                             "to another location.")
+    parser.add_argument("--compare", dest="comparison_location", type=str, help="If supplied will compare your location"
+                                                                                "to another location.")
+    parser.add_argument("-r", "--report", dest="report", choices=["full", "short"], type=str, default='full')
 
     userdata = Inputdata()
     parser.parse_args(namespace=userdata)
